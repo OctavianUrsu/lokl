48ca8d9054cb50f2388106acf23a5c7807960c0e
OctavianUrsu <phenomenno@gmail.com>
2026-04-24 18:22:31 +0200

    Add login page
    Email/password login with Supabase auth, redirects to / on success.

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+
+	let { form } = $props();
+</script>
+
+<h1>Log In</h1>
+
+{#if form?.error}
+	<p style="color: red">{form.error}</p>
+{/if}
+
+<form method="POST" use:enhance>
+	<div>
+		<label for="email">Email</label>
+		<input type="email" name="email" id="email" required />
+	</div>
+	<div>
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password" required />
+	</div>
+	<button type="submit">Log In</button>
+</form>
+<p>Don't have an account? <a href="/signup">Sign up</a></p>
